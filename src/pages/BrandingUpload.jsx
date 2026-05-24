9d9279cde0e64d15993ad85dac834ac615588ad0
Wittemberg <wittemberg@hotmail.com>
2026-05-24 12:36:29 -0300

    fix: correção branding upload3

--- src/pages/BrandingUpload.jsx
+++ src/pages/BrandingUpload.jsx
@@ -408,7 +408,11 @@ export default function BrandingUpload({ onBrandingApplied, currentSettings }) {
 
           <SitePreview colors={colors} logoUrl={logoUrl} localPreview={preview} />
         </div>
+        
       </div>
+
     </div>
+
   )
+
 }
